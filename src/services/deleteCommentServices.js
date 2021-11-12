e8310433ef1e638624390122ad35edce77a7cdc7
ZahraNejati <nejatizahra4@gmail.com>
2021-11-12 07:27:57 +0330

    handle new baseURL axios.create

--- src/services/deleteCommentServices.js
+++ src/services/deleteCommentServices.js
@@ -2,3 +2,7 @@ import http from "./httpServices";
 export function deleteComment(id) {
   return http.delete(`/comments/${id}`);
 }
+// import instance from "./axiosInstance";
+// export function deleteComment(id) {
+//   return instance.delete(`/comments/${id}`);
+// }
